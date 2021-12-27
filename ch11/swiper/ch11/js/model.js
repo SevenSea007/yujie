3428ccbc4fd02ea637dd7c875a30e5c4d75322a3
SevenSea007 <2172775276@qq.com>
2021-12-27 16:07:34 +0800

    第十一周项目文件

---
+++ ch11/swiper/ch11/js/model.js
@@ -0,0 +1,10 @@
+$(function() {
+    //打开模块框
+    $('#btn').click(function() {
+        $('.model').fadeIn();
+    });
+    //关闭模块框
+    $('#close').click(function() {
+        $('.model').fadeOut();
+    })
+})
